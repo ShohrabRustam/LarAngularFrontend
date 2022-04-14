556107f2af40be29838e1da680be6b56456ecb0c
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-14 13:38:42 +0530

    routing in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
-    path:'login',
-    component:LoginComponent
+    path:'',
+    component:HomeComponent
   },
   {
     path: 'register',
     component:RegisterComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
   }
 ];
 
