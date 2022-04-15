e6a77943f143cea9ac92e86f4d85150f037ed9dd
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-15 19:24:12 +0530

    auth laravel with angular working

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import jwt_decode from 'jwt-decode';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  token : any;
+  userData:any;
+  email:any;
+  constructor(private router:Router) { }
+
+  logout(){
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 
   ngOnInit(): void {
+    this.token= localStorage.getItem('token');
+    this.userData= jwt_decode(this.token);
+    this.email=this.userData.email;
+    console.log(this.token);
+
+    console.log(this.userData.email);
   }
 
 }
