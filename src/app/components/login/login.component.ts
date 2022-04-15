433565fa58c80cbe0e5a78530d0980b7f18c5c66
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-15 18:16:42 +0530

    setup the login form with validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
 import { DataService } from 'src/app/services/data.service';
 import { ToastrService } from 'ngx-toastr';
 @Component({
@@ -18,9 +18,19 @@ export class LoginComponent implements OnInit {
     return this.form.controls;
   }
 
-  constructor(private formBuilder:FormBuilder, private toastrService:ToastrService, private dataService:DataService ) { }
+  constructor(private formBuilder:FormBuilder, private toastrService:ToastrService, private dataService:DataService )
+   { }
+
+   createForm(){
+    this.form=this.formBuilder.group({
+      email:[null,[Validators.required,Validators.email]],
+      password:[null,[Validators.required,Validators.minLength(6)]],
+    })
+  }
+
 
   ngOnInit(): void {
+    this.createForm();
   }
 
   submit(){
@@ -31,6 +41,8 @@ export class LoginComponent implements OnInit {
       this.dataService.loginUser(this.form.value).subscribe(
         res=>{
           this.data=res;
+          console.log(res);
+          // return;
           if(this.data.status===1){
 
           }
