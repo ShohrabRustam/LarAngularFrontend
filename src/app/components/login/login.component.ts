6c6011658ee15094a3ec9abac4e4c3976118745c
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-15 13:34:21 +0530

    form group in login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  form:any;
   constructor() { }
 
   ngOnInit(): void {
